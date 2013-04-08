54dcc55248787745b87a2663fd1d472aba2d3037
kpdecker <kpdecker@gmail.com>
2013-04-08 13:41:49 -0500

    Simplify test declaration

--- test/test.historytracker.js
+++ test/test.historytracker.js
@@ -3,6 +3,8 @@ $(document).ready(function() {
   var router = null;
   var lastRoute = null;
   var lastArgs = [];
+  var steps = [];
+  var startingIndex;
 
   function onRoute(router, route, args) {
     lastRoute = route;
@@ -22,6 +24,32 @@ $(document).ready(function() {
     Backbone.history.bind('route', handler);
   }
 
+  function step(page, direction, offset, callback) {
+    var index = steps.length;
+    steps[index] = function(router, name, params) {
+      var hist = Backbone.history;
+      equals(hist.getFragment(), 'search/manhattan/p' + page);
+      equals(params.direction, direction, 'route direction');
+      equals(hist.getIndex(), startingIndex+offset, 'history index');
+      equals(router.query, 'manhattan');
+      equals(router.page, page);
+
+      if (steps[index + 1]) {
+        routeBind(steps[index + 1]);
+      }
+      setTimeout(callback, 0);
+    };
+  }
+
+  function cleanup() {
+    if (window.testPushState) {
+      window.history.pushState({}, document.title, originalUrl);
+    } else {
+      window.location.hash = '';
+    }
+    start();
+  }
+
   module("Backbone.Historytracker", {
 
     setup: function() {
@@ -32,6 +60,9 @@ $(document).ready(function() {
       lastRoute = null;
       lastArgs = [];
       Backbone.history.on('route', onRoute);
+
+      steps = [];
+      startingIndex = Backbone.history.getIndex();
     },
 
     teardown: function() {
@@ -53,124 +84,32 @@ $(document).ready(function() {
     }
   });
 
-  asyncTest("Router: index delta", 32, function() {
-    var startingIndex = Backbone.history.getIndex();
+  asyncTest("Router: index delta", 40, function() {
+    step('20', 1, 1, function() {
+      Backbone.history.navigate('search/manhattan/p30', {replace: true, trigger: true});
+    });
+    step('30', 0, 1, function() {
+      Backbone.history.navigate('search/manhattan/p40', true);
+    });
+    step('40', 1, 2, function() {
+      Backbone.history.back(true);
+    });
+    step('30', -1, 1, function() {
+      Backbone.history.navigate('search/manhattan/p50', true);
+    });
+    step('50', 1, 2, function() {
+      Backbone.history.navigate('search/manhattan/p60', true);
+    });
+    step('60', 1, 3, function() {
+      Backbone.history.go(-2, false);
+      Backbone.history.navigate('search/manhattan/p70', true);
+    });
+    step('70', 1, 2, function() {
+      Backbone.history.back(true);
+    });
+    step('30', -1, 1, cleanup);
 
-    function step1(router, name, params) {
-      console.log(arguments);
-      var hist = Backbone.history;
-      equals(hist.getFragment(), 'search/manhattan/p20');
-      equals(params.direction, 1);
-      equals(hist.getIndex(), startingIndex+1);
-      equals(router.query, 'manhattan');
-      equals(router.page, '20');
-
-      routeBind(step2);
-
-      setTimeout(function() {
-        Backbone.history.navigate('search/manhattan/p30', {replace: true, trigger: true});
-      }, 0);
-    }
-    function step2(router, name, params) {
-      var hist = Backbone.history;
-      equals(hist.getFragment(), 'search/manhattan/p30');
-      equals(params.direction, 0);
-      equals(hist.getIndex(), startingIndex+1);
-      equals(router.query, 'manhattan');
-      equals(router.page, '30');
-
-      routeBind(step3);
-
-      setTimeout(function() {
-        Backbone.history.navigate('search/manhattan/p40', true);
-      }, 0);
-    }
-    function step3(router, name, params) {
-      var hist = Backbone.history;
-      equals(hist.getFragment(), 'search/manhattan/p40');
-      equals(params.direction, 1);
-      equals(hist.getIndex(), startingIndex+2);
-      equals(router.query, 'manhattan');
-      equals(router.page, '40');
-
-      routeBind(step4);
-
-      setTimeout(function() {
-        Backbone.history.back(true);
-      }, 0);
-    }
-    function step4(router, name, params) {
-      var hist = Backbone.history;
-      equals(hist.getFragment(), 'search/manhattan/p30');
-      equals(params.direction, -1);
-      equals(hist.getIndex(), startingIndex+1);
-      equals(router.query, 'manhattan');
-      equals(router.page, '30');
-
-      routeBind(step5);
-
-      setTimeout(function() {
-        Backbone.history.navigate('search/manhattan/p50', true);
-      }, 0);
-    }
-    function step5(router, name, params) {
-      var hist = Backbone.history;
-      equals(hist.getFragment(), 'search/manhattan/p50');
-      equals(params.direction, 1);
-      equals(hist.getIndex(), startingIndex+2);
-
-      routeBind(step6);
-
-      setTimeout(function() {
-        Backbone.history.navigate('search/manhattan/p60', true);
-      }, 0);
-    }
-    function step6(router, name, params) {
-      var hist = Backbone.history;
-      equals(hist.getFragment(), 'search/manhattan/p60');
-      equals(params.direction, 1);
-      equals(hist.getIndex(), startingIndex+3);
-
-      routeBind(step7);
-
-      setTimeout(function() {
-        Backbone.history.go(-2, false);
-        Backbone.history.navigate('search/manhattan/p70', true);
-      }, 0);
-    }
-
-    function step7(router, name, params) {
-      var hist = Backbone.history;
-      equals(hist.getFragment(), 'search/manhattan/p70');
-      equals(params.direction, 1);
-      equals(hist.getIndex(), startingIndex+2);
-
-      routeBind(step8);
-
-      setTimeout(function() {
-        Backbone.history.back(true);
-      }, 0);
-    }
-
-    function step8(router, name, params) {
-      var hist = Backbone.history;
-      equals(hist.getFragment(), 'search/manhattan/p30');
-      equals(params.direction, -1);
-      equals(hist.getIndex(), startingIndex+1);
-
-      setTimeout(cleanup, 0);
-    }
-
-    function cleanup() {
-      if (window.testPushState) {
-        window.history.pushState({}, document.title, originalUrl);
-      } else {
-        window.location.hash = '';
-      }
-      start();
-    }
-
-    routeBind(step1);
+    routeBind(steps[0]);
     setTimeout(function() {
       Backbone.history.navigate('search/manhattan/p20', true);
     }, 0);
